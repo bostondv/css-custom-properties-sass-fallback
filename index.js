745cc09f44f6073a7134a2c04891d87719db1d81
Boston Dell-Vandenberg <boston@pomelodesign.com>
2016-10-14 17:49:33 -0400

    Support adjusting color

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ var sass = require('node-sass');
 var output;
 var css = fs.readFileSync('input.scss', 'utf8');
 
-output = postcss()
-  .use(customProperties({preserve: true}))
-  .use(discardDuplicates())
-  .process(css)
-  .css;
-
 output = sass.renderSync({
-  data: output,
+  data: css,
   outputStyle: 'expanded'
 });
 
 output = output.css.toString('utf-8');
+
+output = postcss()
+  .use(customProperties({preserve: true}))
+  .use(discardDuplicates())
+  .process(output)
+  .css;
 
 fs.writeFileSync('output.css', output, 'utf-8', function(err) {
   if (err) throw err;
